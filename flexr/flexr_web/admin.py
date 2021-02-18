f3e5c334dcbf90430a0fc5ef205068a026445a4c
Tyler <tuh26432@temple.edu>
2021-02-17 23:27:00 -0500

    database initialized

--- flexr/flexr_web/admin.py
+++ flexr/flexr_web/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import *
+
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Account)
+admin.site.register(Team)
+admin.site.register(Site)
+admin.site.register(Tab)
+admin.site.register(Bookmark)
+admin.site.register(Device)
+admin.site.register(Account_Preferences)
+admin.site.register(Note)
