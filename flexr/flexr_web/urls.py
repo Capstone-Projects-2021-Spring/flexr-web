c2c5fbc2176dec827284651a899452805a810df0
Faris Awad <tuk86431@temple.edu>
2021-03-12 02:37:42 -0500

    create note working

--- flexr/flexr_web/urls.py
+++ flexr/flexr_web/urls.py
@@ -13,6 +13,10 @@ urlpatterns = [
     path('api/tabs/', AllTabsView.as_view()),
     path('api/tab/<id>', TabView.as_view()),
     path('api/tab/', TabView.as_view()),
+    path('create_note', create_note, name='create_note'),
+    path('opennote/<int:pk>/', note_individual_web.as_view(), name='note-detail'),
+
+
     #path('shared_folder/', ) TODO 
     # TODO For note.html
 ]
