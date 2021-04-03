d3ac0b1b44915b317042e1456a63251bf8b4df3e
pushkinroye <pushkin.roye@gmail.com>
2021-04-03 13:51:27 -0400

    edit bug fixed, improved UI for shared folders

--- flexr/flexr_web/class_views/SharedFolderView.py
+++ flexr/flexr_web/class_views/SharedFolderView.py
@@ -2,7 +2,10 @@ from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import redirect, render
 from django.views import View
+from django.views.generic import DetailView
+from django.http import HttpResponse, JsonResponse
 
+import json
 import pytz
 
 from ..models import *
@@ -61,58 +64,57 @@ class SharedFolderView(LoginRequiredMixin, View):
         current_acc = request.user.accounts.get(account_id=request.session['account_id'])
         shared_folder = current_acc.shared_folders.get(id=kwargs['pk'])
         print(request.POST.get("new_title"))
-        shared_folder.title = request.POST.get("new_title")
-        shared_folder.title = request.POST.get("new_description")
-        # form = EditSharedFolder(request.POST)
-        #
-        # collaborators = obj.collaborators.all()
-        # tabs = obj.tabs.all()
-        # bookmarks = obj.bookmarks.all()
-        # notes = obj.notes.all()
+        # shared_folder.title = request.POST.get("new_title")
+        # shared_folder.title = request.POST.get("new_description")
         form = EditSharedFolder(request.POST)
         if form.is_valid():
+            shared_folder.title = request.POST.get("new_title")
+            shared_folder.title = request.POST.get("new_description")
             print("\n\n\n\nit'svalid")
             shared_folder.title = request.POST.get('title')
-
+            shared_folder.description = request.POST.get('description')
+            tileo = form.cleaned_data['title']
+            descrippy = form.cleaned_data['description']
+            collaboratoros = form.cleaned_data['collaborators']
+            bookmarkos = form.cleaned_data['bookmarks']
+            tabos = form.cleaned_data['tabs']
+            noteso = form.cleaned_data['notes']
+            shared_folder.collaborators.set(collaboratoros)
+            shared_folder.bookmarks.set(bookmarkos)
+            shared_folder.notes.set(noteso)
+            shared_folder.tabs.set(tabos)
         shared_folder.save()
         context = {'form': form}
         # return render('shared_folder/' + str(shared_folder.pk))
         return redirect('/shared_folder/' + str(shared_folder.id))
-        # return render(request, "flexr_web/shared_folder.html",
-        #  {"shared_folder": obj,
-        #   "Collaborators": collaborators,
-        #   "Tabs":tabs,
-        #   "Bookmarks": bookmarks,
-        #   "Notes": notes},
-        #   "form: form")
 
-    # def edit_shared_folder(self, request, *args, **kwargs):
-    #     """
-    #     Edit a note
-    #     """
-    #
-    #     # get form object on the page
-    #     form = EditSharedFolder(request.POST)
-    #     # print("Note edited")
-    #     obj = Account.objects.get(account_id=request.session['account_id']).shared_folders.get(pk=kwargs['pk'])
-    #     # check if form is valid
-    #     if form.is_valid():
-    #         # get current account
-    #         curr_acc = Account.objects.get(account_id=request.session['account_id'])
-    #
-    #         # get information from form
-    #         title = request.POST.get('title')
-    #         # content = request.POST.get('content')
-    #
-    #         # get requested note and update with requested data
-    #         obj = curr_acc.shared_folders.get(pk=kwargs['pk'])
-    #         obj.title = title
-    #         obj.content = content
-    #         obj.save()
-    #
-    #         # request messages for debugging
-    #         request.session['message'] = "Note edited"
-    #     request.session['err_message'] = "Note could not be edited"
-    #
-    #     # display requested note after editing
-    #     return redirect('/opennote/' + str(obj.id))
+class FoldersViewAPI(LoginRequiredMixin, DetailView):
+
+    def put(self, request, *args, **kwargs):
+        """
+            Edit
+        """
+        
+        return HttpResponse()
+
+    def post(self, request, *args, **kwargs):
+        """
+            Create
+        """
+        current_acc = request.user
+        current_acc = current_acc.accounts.get(account_id = request.session['account_id'])
+        data = json.loads(request.body)
+        # data = folderSerializer(to be written and finalized)
+        return HttpResponse()
+
+    def get(self, request, *args, **kwargs):
+        current_acc = request.user.accounts.get(account_id=request.session['account_id'])
+        shared_folder = current_acc.shared_folders.get(id=kwargs['pk'])
+        shared_folder = sharedFolder.objects.filter(owner = current_acc, description=shared_folder.description)
+        return HttpResponse()
+
+    def delete(self, request, *args, **kwargs):
+        current_acc = request.user.accounts.get(account_id=request.session['account_id'])
+        shared_folder = current_acc.shared_folders.get(id=kwargs['pk'])
+        shared_folder = sharedFolder.objects.filter(owner = current_acc, description=shared_folder.description)
+        return JsonResponse({"successful": "folder deleted"})
