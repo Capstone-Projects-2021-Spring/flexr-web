29367db2ee6498808ede95fbfa9d71b96f7df256
Gerald Whitters <whittersgerald@gmail.com>
2021-04-06 04:18:50 -0400

    allow to switch accounts

--- extension/accounts.js
+++ extension/accounts.js
@@ -7,49 +7,47 @@ window.onload = function () {
         window.location.href = '/menu.html'
     }
 
-    let get_tabs = new Promise(function(resolve, reject){
-        var request = new XMLHttpRequest();
-            
-        request.open("GET", "http://127.0.0.1:8000/api/tabs/");
-        //request.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-        //request.send("username=admin2&password=password");
-        request.send();
-        request.onload = () => {
-            console.log(request);
-            if (request.status == 200){
-                bkg.console.log(JSON.parse(request.response));
-                //userid = JSON.parse(request.response).id
-                accounts = JSON.parse(request.response)
-                
-                resolve('Promise is resolved successfully.')
+    async function get_accounts(){
+        accounts_response = await fetch('http://127.0.0.1:8000/api/accounts/');
 
-            }
-            else{
-                bkg.console.log(`error ${request.status} ${request.statusText}`);
-                reject('Promise is rejected');  
-            }
+        accounts = await accounts_response.json();
+
+        display_accounts();
+    }
+
+    async function switch_account(accountid){
+        let switch_response = await fetch(`http://127.0.0.1:8000/api/account/${accountid}/switch/`);
+
+        if(switch_response.ok){
+            bkg.console.log('switched to account with account id:' + accountid);
         }
-    })
-
-    get_tabs.then(display_accounts)
+        else{
+            bkg.console.log('error');
+        }
+    }
 
 
     function display_accounts(){
-
-        for(i = 0; i < tabs.length; i++){
-            bkg.console.log(accounts[i].username);
+        for(i = 0; i < accounts.length; i++){
+            bkg.console.log(accounts[i].username + ' ' + accounts[i].account_id);
 
             var parent = document.createElement('p');
-            var a = document.createElement('a');
-            var linkText = document.createTextNode(accounts[i].username);
-            a.appendChild(linkText);
-            a.href = accounts[i].username;
+            button = document.createElement('button');
+            var text = document.createTextNode(accounts[i].username);
+            button.appendChild(text);
+            
+            
+            button.onclick = function(index){
+                return function(){switch_account(accounts[index].account_id)}
+            }(i);
 
-            parent.appendChild(a);
+            parent.appendChild(button);
             document.body.appendChild(parent);
+               
+            
         }
-
-        
-
+    
     }
+
+    get_accounts();
 }
