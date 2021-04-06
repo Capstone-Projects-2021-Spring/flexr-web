90d6bc6a07893aa8da489ea8e14e728162d10de9
Gerald Whitters <whittersgerald@gmail.com>
2021-04-06 05:00:26 -0400

    init for tracking tabs

--- extension/tabs.js
+++ extension/tabs.js
@@ -1,10 +1,22 @@
 window.onload = function () {
     let backButton = document.getElementById('backButton');
+    let trackButton = document.getElementById('trackButton');
     let bkg = chrome.extension.getBackgroundPage();
     let tabs = []
     
     backButton.onclick = function() {
         window.location.href = '/menu.html'
+    }
+
+    trackButton.onclick = function(){
+        chrome.tabs.query({active: true, currentWindow: true}, function(T) {
+            var activeTab = T[0];
+            
+
+            bkg.console.log(activeTab.url)
+            bkg.console.log(activeTab.id)
+       
+         });
     }
 
     async function get_tabs(){
@@ -18,7 +30,7 @@ window.onload = function () {
     function display_tabs(){
 
         for(i = 0; i < tabs.length; i++){
-            bkg.console.log(tabs[i].url);
+            //bkg.console.log(tabs[i].url);
 
             var parent = document.createElement('p');
             var a = document.createElement('a');
