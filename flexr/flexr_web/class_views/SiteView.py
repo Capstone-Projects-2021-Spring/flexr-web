3af5f4eaaca8638aff5fd3e65c231df455b6c2ae
Gerald Whitters <whittersgerald@gmail.com>
2021-04-23 09:41:29 -0400

    exceptions for site api

--- flexr/flexr_web/class_views/SiteView.py
+++ flexr/flexr_web/class_views/SiteView.py
@@ -8,6 +8,7 @@ from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 import pytz
+import traceback
 
 from ..models import *
 from ..forms import *
@@ -18,22 +19,29 @@ from ..serializers import SiteSerializer
 class SiteAPIView(LoginRequiredMixin, View):
 
     def get(self, request):
-        curr_account = request.user.accounts.get(account_id = request.session['account_id'])
-        print("SiteAPIView: get(): request.GET.get('url')", request.GET.get("url"))
-        site = curr_account.sites.get(url = request.GET.get('url') )
-        data = SiteSerializer(site)
-        return JsonResponse(data.data, safe=False)
+        try:
+            curr_account = request.user.accounts.get(account_id = request.session['account_id'])
+            print("SiteAPIView: get(): request.GET.get('url')", request.GET.get("url"))
+            site = curr_account.sites.get(url = request.GET.get('url') )
+            data = SiteSerializer(site)
+            return JsonResponse(data.data, safe=False)
+        except Exception as e:
+            return JsonResponse({ "error": str(e), "traceback": traceback.extract_tb(e.__traceback__).format() }, status=500)
 
     def post(self, request):
-        curr_user = request.user
-        curr_account = curr_user.accounts.get(account_id=request.session['account_id'])
+        try:
+            curr_user = request.user
+            curr_account = curr_user.accounts.get(account_id=request.session['account_id'])
 
-        url = request.POST.get('url')
-        site, created = Site.objects.get_or_create(account = curr_account, url = url)
-        site.save()
+            url = request.POST.get('url')
+            site, created = Site.objects.get_or_create(account = curr_account, url = url)
+            site.save()
 
-        data = SiteSerializer(site)
-        return JsonResponse(data.data, safe=False)
+            data = SiteSerializer(site)
+            return JsonResponse(data.data, safe=False)
+
+        except Exception as e:
+            return JsonResponse({ "error": str(e), "traceback": traceback.extract_tb(e.__traceback__).format() }, status=500)
 
     def put(self):
         pass
