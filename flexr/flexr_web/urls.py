7ec7d0e9bdefb177c9754078d327f0c959a4cd54
Faris Awad <tuk86431@temple.edu>
2021-04-25 21:13:33 -0400

    adding bookmarks to bookmark

--- flexr/flexr_web/urls.py
+++ flexr/flexr_web/urls.py
@@ -60,6 +60,7 @@ urlpatterns = [
     path('delete_bookmark_folder/<int:pk>/', BookmarkFolderView().delete_bookmark_folder_web, name='delete-bookmark-folder'),
     path('edit_bookmark_folder/<int:pk>', BookmarkFolderView().edit_bookmark_folder, name='edit_bookmark_folder'),
     path('remove_from_folder/<int:pk>/<int:id>/', BookmarkFolderView().remove_from_folder, name='remove_from_folder'),
+    path('bookmark_folder/<id>/add_bookmark/', BookmarkFolderView().add_bookmark, name = "add bookmark bookmark"),
 
     path('shared_folders/', SharedFoldersView.as_view(), name = "shared folders"),
     path('add_shared_folder/', SharedFoldersView().create_shared_folder, name = "add shared folder"),
